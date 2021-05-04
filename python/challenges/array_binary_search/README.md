4333c10799173cb7f97e6444c7b7f0c8a4d5583e
m7madmomani2 <mohammad.momani1996.kk@gmail.com>
2021-05-04 23:11:15 +0300

    solve challenge

---
+++ python/challenges/array_binary_search/README.md
@@ -0,0 +1,16 @@
+# array_binary_search
+> Binary Search is a Divide and Conquer algorithm. binary search first divides a large array into two smaller subarrays and then recursively operates the subarrays. But instead of operating on both subarrays, it discards one subarray and continues on the second subarray until it finds the target
+
+## Edge Cases
+> does not support non-integers, return an error
+
+
+## Challenge
+> The problem that we have a list and we need to find value in with lowest Big O
+
+
+## Approach & Efficiency
+> I chose to use Recursion to divided the array and middle method to get the element of the middle of array,each time it dvided the array by 2 until find the element or if not exiset it return Not found
+
+## Solution
+[array_binary_search](https://miro.com/app/board/o9J_lG44R2c=/)
---
+++ python/tests/test_array_binary_search.py
@@ -0,0 +1,65 @@
+from challenges.array_binary_search import __version__
+from challenges.array_binary_search.array_binary_search import *
+def test_version():
+    assert __version__=='0.1.0'
+
+def test_binary_search_tree_string():
+    userinput1=[[1, 2, 3, 10, 4, 5], "5"]
+    userinput2=[[1, "2", 3, 4, 5], 5]
+    exOutput1 = "Type Error"
+    assert binary_search_tree(userinput1[0],userinput1[1]) == exOutput1
+    assert binary_search_tree(userinput2[0],userinput2[1]) == exOutput1
+
+# def test_binary_search_tree_string():
+#     userinput1=[[1,2,3,4,5,7,10,15,20,30], 2]
+#     userinput2=[[1,2,3,4,5,7,10,15,20,30], 7]
+#     userinput3=[[1,2,3,4,5,7,10,15,20,30], 30]
+#     userinput4=[[1,2,3,4,5,7,10,15,20,30], 0]
+#     userinput5=[[1,2,3,4,5,7,10,15,20,30], 8]
+#     userinput6=[[1,2,3,4,5,7,10,15,20,30], 25]
+
+#     exOutput1 = True
+#     exOutput2 = False
+#     assert binary_search_tree(userinput1[0],userinput1[1]) == exOutput1
+#     assert binary_search_tree(userinput2[0],userinput2[1]) == exOutput1
+#     assert binary_search_tree(userinput3[0],userinput3[1]) == exOutput1
+#     assert binary_search_tree(userinput4[0],userinput4[1]) == exOutput2
+#     assert binary_search_tree(userinput5[0],userinput5[1]) == exOutput2
+#     assert binary_search_tree(userinput6[0],userinput6[1]) == exOutput2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def test_binary_search_tree_():
+    assert True
+
+def test_binary_search_tree_():
+    assert True
+
+def test_binary_search_tree_():
+    assert True
+
+def test_binary_search_tree_():
+    assert True
+
+def test_binary_search_tree_():
+    assert True
+
+
+
+
+
+
+
+
