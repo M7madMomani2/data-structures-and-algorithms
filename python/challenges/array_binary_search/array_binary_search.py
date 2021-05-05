b364049874374e072aac9596bb822164b2684d32
m7madmomani2 <mohammad.momani1996.kk@gmail.com>
2021-05-05 15:56:11 +0300

    solve error

--- python/challenges/array_binary_search/array_binary_search.py
+++ python/challenges/array_binary_search/array_binary_search.py
@@ -1,11 +1,11 @@
 if __name__ == "__main__":
     riddle_index = 0
 
-def binary_search_tree(arr,n) :
+def binary_search(arr,n) :
 
     if len(arr)==0:
         # print("Not Found")
-        return False
+        return -1
 
     elif (not all(isinstance(i, int) for i in arr)) or type(n)!=int:
         # print("Type Error")
@@ -13,42 +13,40 @@ def binary_search_tree(arr,n) :
 
     elif len(arr)==1:
         if arr[0]==n:
-            # print("Found")
             return True
         else :
-            # print("not Found")
-            return False
+            return -1
 
     elif middle(arr)==n:
-        # print("Found")
         return True
 
     elif middle(arr)>n:
         # print(arr[:len(arr)//2:])
-        binary_search_tree(arr[:len(arr)//2:],n)
+
+        return binary_search(arr[:len(arr)//2:],n)
 
     elif middle(arr)<n:
         # print(arr[len(arr)//2::])
-        binary_search_tree(arr[len(arr)//2::],n)
+        return binary_search(arr[len(arr)//2::],n)
 
     else :
         # print("Not Found")
-        return  False
+        return  -1
 
 
 def middle(array):
     return array[len(array)//2]
 
-# a=[1,2,3,4,5,7,10,15,20,30]
+a=[1,2,3,4,5,7,10,15,20,30]
 
-# print(binary_search_tree([21],21))
-# binary_search_tree(a,2)
-# binary_search_tree(a,3)
-# binary_search_tree(a,4)
-# binary_search_tree(a,5)
-# binary_search_tree(a,7)
+# print(binary_search([21],21))
+print(binary_search(a,2))
+print(binary_search(a,3))
+print(binary_search(a,4))
+print(binary_search(a,5))
+# binary_search(a,7)
 
-# binary_search_tree(a,10)
-# binary_search_tree(a,15)
-# binary_search_tree(a,20)
-# binary_search_tree(a,30)
+# binary_search(a,10)
+# binary_search(a,15)
+# binary_search(a,20)
+# binary_search(a,30)
