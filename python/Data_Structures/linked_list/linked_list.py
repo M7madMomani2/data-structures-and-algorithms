5a6a31d1d674b681f6b729ba62dc1b9f2f61af15
m7madmomani2 <mohammad.momani1996.kk@gmail.com>
2021-05-16 16:39:53 +0300

    solve problem

--- python/Data_Structures/linked_list/linked_list.py
+++ python/Data_Structures/linked_list/linked_list.py
@@ -40,13 +40,60 @@ class Linkedlist:
 
         return f"{ current_string } -> NULL"
 
+    def append(self,value):
+        node = Node(value)
+        current = self.head
+        while current.next :
+            current=current.next
+        current.next =node
+
+    def insertAfter(self ,value, newVal) :
+        node=Node(newVal)
+        if self.head is None:
+            self.head = node
+        else:
+            current = self.head
+            while current.next:
+                print(current==value)
+                if current.value==value:
+                    bef_val=current.next
+                    current.next =node
+                    print(current.next.value)
+                    node.next=bef_val
+                    return node.value
+                current=current.next
+
+
+    def insertBefore(self, value, newVal):
+        newNode = Node(value)
+        if self.head is None:
+            self.head = newNode
+        else:
+            current=self.head
+            if current.value == newVal:
+                newNode.next = self.head
+                self.head = newNode
+            else :
+                while current.next.value != newVal:
+                    current = current.next
+                if current.next.value == newVal:
+                    old = current.next
+                    current.next = newNode
+                    newNode.next = old
+
+
+
 if __name__ == "__main__":
     l_list = Linkedlist()
     l_list.insert(2)
     l_list.insert(3)
     l_list.insert(0)
     l_list.insert(1)
+    l_list.append(15)
+    l_list.insertBefore(3, 7)
+
     print(l_list.includes(2))
     print(l_list.includes(0))
     print(l_list.to_string())
-#Done
+
+
