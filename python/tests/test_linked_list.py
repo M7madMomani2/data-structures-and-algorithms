d2d0d910cabd0bc03a9c6c55b2e231a81e94baab
m7madmomani2 <mohammad.momani1996.kk@gmail.com>
2021-05-18 13:07:52 +0300

    add docstrings

--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -63,6 +63,33 @@ def test_kthFromEnd(linked_list_ob):
     assert actual_output2 == expected_output2
     assert actual_output3 == expected_output3
 
+def test_kthFromEnd(linked_list_ob):
+    actual_output1 =linked_list_ob.kthFromEnd(3)
+    actual_output2=linked_list_ob.kthFromEnd(0)
+    actual_output3 =linked_list_ob.kthFromEnd(10)
+    expected_output1 = 0
+    expected_output2 = 1
+    expected_output3 = "Sorry invalid input"
+    assert actual_output1 == expected_output1
+    assert actual_output2 == expected_output2
+    assert actual_output3 == expected_output3
+
+def test_zipLists(linked_list_ob,linked_list_ob2):
+    """
+    this function will take tow list and return new one that contain all element from list 1 and list 2
+    by order one node from list and one node from list 2
+    """
+    actual_output1 =linked_list_ob.zipLists(linked_list_ob2).to_string()
+    expected_output1=" {0} -> {1} -> {3} -> {2} -> {2} -> {3} -> {1} -> {0} -> NULL"
+    assert actual_output1== expected_output1
+    linked_list_ob2.insert(5)
+    actual_output2 =linked_list_ob.zipLists(linked_list_ob2)
+    expected_output2="Sorry invalid input"
+    assert actual_output2== expected_output2
+
+
+    print(actual_output1)
+    # print()
 
 
 @pytest.fixture
@@ -74,3 +101,11 @@ def linked_list_ob():
     linked_list_o.insert(0)
     return linked_list_o
 
+@pytest.fixture
+def linked_list_ob2():
+    linked_list_o2=Linkedlist()
+    linked_list_o2.insert(0)
+    linked_list_o2.insert(3)
+    linked_list_o2.insert(2)
+    linked_list_o2.insert(1)
+    return linked_list_o2
