a6ae151212fb8e896280d09bf1cb2abbac58771d
m7madmomani2 <mohammad.momani1996.kk@gmail.com>
2021-06-15 08:26:23 +0300

    solve challenge

--- python/tests/test_merge_sort.py
+++ python/tests/test_merge_sort.py
@@ -1,25 +1,13 @@
-def InsertionSort(arr):
-    for i in range(1, len(arr)):
-        j = i - 1
-        key = arr[i]
+from challenges.merge_sort.merge_sort import Merge ,Mergesort
 
-        while j >= 0 and key < arr[j]:
-            arr[j + 1] = arr[j]
-            j = j - 1
+def test_insertion_sort():
+    userinput = [8,4,23,42,16,15]
+    expected = [4, 8, 15, 16, 23, 42]
+    Mergesort(userinput)
+    assert userinput == expected
 
-        arr[j+1] = key
-
-
-list1 = [8,4,23,42,16,15]
-list2 = [20,18,12,8,5,-2]
-list3 = [5,12,7,5,5,7]
-list4 = [2,3,5,7,13,11]
-
-InsertionSort(list1)
-InsertionSort(list2)
-InsertionSort(list3)
-InsertionSort(list4)
-print(list1)
-print(list2)
-print(list3)
-print(list4)
+def test_insertionSort2():
+    userinput = [20, 41, 3, 2, 16, 10]
+    expected = [2, 3, 10, 16, 20, 41]
+    Mergesort(userinput)
+    assert userinput == expected
